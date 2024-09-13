11b076183bca2b1fbfc1f977544e9f201e1953e9
Anders Sanderli <anders.sanderli@iths.se>
2024-09-13 15:00:40 +0200

    updated README.md

--- Code-Along/counting.py
+++
@@ -1,9 +0,0 @@
-# Simulation on teaching my daughter how to count to ten. 
-
-c = 0
-
-print("Kom igen Leah, det här klarar du.")
-while c <= 10:
-    print(c, end=", ")
-    c += 1
-print("\nFantastiskt!")
--- Labs/lab1.md
+++
@@ -1,6 +0,0 @@
-# Laboration 1
-
-**Objectives:**
-- learn Git and GitHub
-- learn how to submit reports through the school platform
-- learn markdown
