ba02100918f68ddce27de096da731ee7e2c56ccb
Anders Sanderli <anders.sanderli@iths.se>
2024-10-11 13:15:37 +0200

    a first draft of the script is finished, as far as assignments for grade G

--- Labs/lab3/lab3.py
+++ Labs/lab3/lab3.py
@@ -4,9 +4,17 @@ import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
 matplotlib.use('qt5agg')
+import os
+
+# global variables
+read_path = os.path.dirname(__file__) + "/unlabelled_data.csv"
+write_path = os.path.dirname(__file__) + "/labelled_data.csv"
+K = -1
+X = np.linspace(-10, 10, 600)
+M = 0
 
 
-def read_file(filename):                        # returns df with columns x and y
+def read_file(filename):                        # returns df, a dataframe with columns x and y
     
     df = pd.read_csv(filename, header = None)
     df.columns = ["x", "y"]
@@ -14,7 +22,7 @@ def read_file(filename):                        # returns df with columns x and 
     return df
 
 
-def create_figure(df):                          # returns ax plot object
+def create_figure(df):                          # returns ax, a plot object
 
     fig, ax = plt.subplots(figsize=(9, 9))
     ax.set_facecolor("azure")
@@ -24,14 +32,13 @@ def create_figure(df):                          # returns ax plot object
     ax.axvline(x = 0, color = "lightseagreen", linewidth = 0.5, zorder = 2)
 
     max_limit = max(abs(df.values.min()), abs(df.values.max()))             # dessa rader fick jag från Claude, behöver dubbelkollas. gör att plot center är origo
-    ax.set_xlim(-max_limit, max_limit)                                      # dessa rader fick jag från Claude, behöver dubbelkollas. gör att plot center är origo
-    ax.set_ylim(-max_limit, max_limit)                                      # dessa rader fick jag från Claude, behöver dubbelkollas. gör att plot center är origo
+    ax.set_xlim(-max_limit - 0.5, max_limit + 0.5)                                      # dessa rader fick jag från Claude, behöver dubbelkollas. gör att plot center är origo
+    ax.set_ylim(-max_limit -0.5 , max_limit + 0.5)                                      # dessa rader fick jag från Claude, behöver dubbelkollas. gör att plot center är origo
     
     ax.set_title("A scatter plot of the dataframe")
     ax.set_xlabel("x-axis")
     ax.set_ylabel("y-axis")
     ax.grid(True, color="gainsboro", linestyle="dashed", zorder=1)
-    # ax.legend(loc = "upper left")
 
     return ax
 
@@ -41,21 +48,67 @@ def plot_line(X, K, M):                         # plots a regression line
     Y = K * X + M
     ax.plot(X, Y, color = "paleturquoise", label = (f"y = {K}x + {M}"), linewidth = 3, zorder = 1)
     ax.legend(loc = "upper left")
+
+
+def classifier(df, K, M):                       # returns lbl, a list of labels
+    
+    lbl = []
+
+    for i in range(len(df)):
+        x = df.iloc[i][0]
+        y = K * x + M
+        if df.iloc[i][1] < y:
+            lbl.append(0)
+        else:
+            lbl.append(1)
+    
+    return lbl
+
+
+def plot_scatter(df, lbl):                      # plots a scatter plot with labels
+
+    ax.scatter([], [], color = "aquamarine", edgecolors = "lightseagreen", label = "class 0", linewidth = 1, s = 50, alpha = 0.75, zorder = 3)
+    ax.scatter([], [], marker = "H", color = "turquoise", edgecolors = "teal", label = "class 1", linewidth = 1, s = 50, alpha = 0.75, zorder = 3)
+
+    for i in range(len(df)):
+        if lbl[i] == 0:
+            ax.scatter(df.iloc[i][0], df.iloc[i][1], color = "aquamarine", edgecolors = "lightseagreen", linewidth = 1, s = 50, alpha = 0.75, zorder = 3)
+        if lbl[i] == 1:
+            ax.scatter(df.iloc[i][0], df.iloc[i][1], marker = "H", color = "turquoise", edgecolors = "teal", linewidth = 1, s = 50, alpha = 0.75, zorder = 3)
+
+    ax.legend(loc = "upper left")
+
+
+def write_file(df, lbl):                        # returns df_l, a dataframe with columns x, y, label
+    
+    df_l = df.copy()
+    df_l["label"] = lbl
+    df_l.to_csv(write_path, index = False, header = False)
+
+    return df_l
+
+
+# main
+if __name__ == "__main__":
+    dataframe = read_file(read_path)
+    ax = create_figure(dataframe)
+    plot_line(X, K, M)
+    labels = classifier(dataframe, K, M)
+    plot_scatter(dataframe, labels)
+    dataframe_labelled = write_file(dataframe, labels)
     plt.show()
 
 
-# function
 
 
-# other
+# NOTES
+# 1. läs fil
+# 2. initiera globala variabler 
+# 3. klassificera
+# 4. skriv fil
+# 5. plotta graf, punkter, linje
 
-# global variables
-file_path = "/home/albot/coding/repos/python-programming-ANDERS-SANDERLI/Labs/lab3/unlabelled_data.csv"
-K = -1
-X = np.linspace(-10, 10, 600)
-M = 0
 
-# main
-dataframe = read_file(file_path)
-ax = create_figure(dataframe)
-plot_line(X, K, M)
+# FIX
+# ändra linspace till att vara len(dataframe) i stället för 600 - dubbelkolla bara så att inte headers räknas med
+# ändra ordningen så att den matchar NOTES
